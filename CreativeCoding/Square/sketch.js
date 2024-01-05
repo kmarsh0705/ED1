d00a56b0aa4143f148a156bf17bea7131354f07d
kmarsh0705 <kirandmarsh@gmail.com>
2024-01-05 11:20:44 -0500

    aadsf

--- CreativeCoding/Square/sketch.js
+++ CreativeCoding/Square/sketch.js
@@ -1,7 +1,16 @@
-function setup() {// runs once
-
+function setup() {
+  createCanvas(720, 720);
+    background('blue');
+  strokeWeight(8);
 }
 
-function draw() {//runs in a loop
-
+function draw() {
+  
+  translate (60,60); //creates a new origin point
+quad (
+  0,0,
+  300,0,
+  300,300,
+  0,300
+);
 }
