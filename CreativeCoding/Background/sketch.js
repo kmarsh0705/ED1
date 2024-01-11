dc51aa785063b2734c5537eb50f12de7b7c4ab29
kmarsh0705 <kirandmarsh@gmail.com>
2024-01-11 14:21:50 -0500

    update

--- CreativeCoding/Background/sketch.js
+++ CreativeCoding/Background/sketch.js
@@ -1,8 +1,44 @@
+
+let gif;
+
+function preload (){
+gif = loadImage ('assets/shift.gif');
+
+}
+
 function setup() {// runs once
-createCanvas (windowWidth, windowHeight);
-background ('cyan') // red background
+	createCanvas(720, 720);
+	background('blue');
+	strokeWeight(8);
+	
+	fill ('pink');
 }
 
 function draw() {//runs in a loop
 
+background('255,255,255');
+
+var num = 20
+
+var sideLen = windowWidth;
+
+var sideLen = windowWidth/num;
+
+	for (var y = 0; y < windowWidth; y = y + sideLen) {
+	
+		for (var x = 0; x < windowWidth; x = x + sideLen) {
+		
+image (gif,x,y, sideLen, sideLen);
+		
 }
+	
+	
+}
+
+//noLoop():
+}
+
+function windowWidth (){
+resizeCanvas (windowWidth, windowHeight);
+}
+
---
+++ CreativeCoding/empty ish/sketch.js
@@ -0,0 +1,8 @@
+function setup() {// runs once
+createCanvas (windowWidth, windowHeight);
+background ('cyan') // red background
+}
+
+function draw() {//runs in a loop
+
+}
