cb20a8b508cce3ef0f7a732f4e12e0bf60d1717e
kmarsh0705 <kirandmarsh@gmail.com>
2024-01-12 08:40:34 -0500

    update

--- CreativeCoding/revisedgif/sketch.js
+++ CreativeCoding/revisedgif/sketch.js
@@ -1,7 +1,29 @@
-function setup() {// runs once
+let gif;
 
+function preload (){
+gif = loadImage('assets/MarshGif.gif');
+}
+
+function setup() {// runs once
+	createCanvas (windowWidth, windowHeight);
+	fill('blue');
+	strokeWeight(1);
 }
 
 function draw() {//runs in a loop
+	background (yellow);
+	var num = 20
+	var sideLen = windowWidth/num;
+	
+	for (var y = 0; y < windowHeight; y = y + sideLen) {
+		
+		for (var x = 0; x< windowWidth; x = x + sideLen){
+		
+		image (gif, x , y, sideLen, sideLen);
+		}
+	}
+}
 
+function windowResized(){
+	resizeCanvas (windowWidth, windowHeight);
 }
