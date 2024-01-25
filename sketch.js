e3a36df850b3423f18e7600e4b81d44c10656784
kmarsh0705 <kirandmarsh@gmail.com>
2024-01-25 11:07:19 -0500

    yfoouyfoufyyfou

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
-function setup() {// runs once
+let gif;
 
+function preload (){
+gif = loadImage ('assets/shift.gif');
+
+}
+
+function setup() {// runs once
+	createCanvas(windowWidth, windowHeight);
+	background('blue');	
+	fill ('pink');
 }
 
 function draw() {//runs in a loop
 
+background('blue');
+
+var num = 20;
+
+var sideLen = windowWidth/num;
+
+	for (var y = 0; y < windowHeight; y = y + sideLen) {
+	
+		for (var x = 0; x < windowWidth; x = x + sideLen) {
+		
+image (gif,x,y, sideLen, sideLen);
+		
 }
+	
+	
+}
+
+}
+
+function windowWidth (){
+resizeCanvas (windowWidth, windowHeight);
+}
+
