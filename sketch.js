b3eeb1bb3651e62eb7df07fb95d79fff741ebccd
kmarsh0705 <kirandmarsh@gmail.com>
2024-01-26 12:59:24 -0500

    hhh

--- sketch.js
+++ sketch.js
@@ -19,9 +19,9 @@ var num = 6;
 
 var sideLen = windowWidth/num;
 
-	for (var y = 0; y < windowHeight; y = y + sideLen) {
+	for (var y = 0; y < 2 * windowHeight; y = y + sideLen) {
 	
-		for (var x = 0; x < windowWidth; x = x + sideLen) {
+		for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
 		
 image (gif,x,y, sideLen, sideLen);
 		
